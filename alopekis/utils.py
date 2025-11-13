3fc5b0f1168c1803ddd4b9edc8794efd955a2170
Mike Bennett <mbennett@datacite.org>
2025-11-13 01:29:16 +0000

    Fix count error + add some logging

--- alopekis/utils.py
+++ alopekis/utils.py
@@ -33,10 +33,12 @@ def get_month_count(year: int, month: int, logger=None) -> int:
 
 def queue_month(year: int, month: int, work_queue: Queue, results_queue: Queue, count: int = None, logger=None) -> None:
     """Queue a month to be processed, retrieving the expected count of records if it is not provided"""
+    logger.debug(f"Queueing job for {year}-{month} with expected count: {count}")
     if count:
         count = int(count)
     else:
-        get_month_count(year, month, logger)
+        logger.debug(f"No count for {year}-{month} provided, querying OpenSearch")
+        count = get_month_count(year, month, logger)
 
     work_queue.put({
         'year': int(year),
